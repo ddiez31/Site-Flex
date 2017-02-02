83ab60caa95051f3488a24505aa60e848ea009ef
thibault <thibault.daboville@gmail.com>
2017-02-02 14:58:09 +0100

    commit avant pull

--- app.js
+++ app.js
@@ -32,25 +32,46 @@ $(document).ready(function() {
                 var outils = $(this).attr("id");
                 console.log(outils);
                 if(outils === "containercolrev"){
-                    $("#TestFlexFlow").css('flex-direction', '');
+                    $("#TestFlexFlow").css('flex-direction', 'column-reverse');
+                    if(!($("#activeFF").is(':checked'))){
+                        $("#TestFlexFlow").css('flex-wrap', '');
+                    }
                 }else if(outils === "containercol"){
                     $("#TestFlexFlow").css('flex-direction', 'column');
+                    if(!($("#activeFF").is(':checked'))){
+                        $("#TestFlexFlow").css('flex-wrap', '');
+                    }
                 }else if(outils === "row"){
                     $("#TestFlexFlow").css('flex-direction', 'row');
+                    if(!($("#activeFF").is(':checked'))){
+                        $("#TestFlexFlow").css('flex-wrap', '');
+                    }
                 }else if(outils === "row-reverse"){
+                    if(!($("#activeFF").is(':checked'))){
+                        $("#TestFlexFlow").css('flex-wrap', '');
+                    }
                     $("#TestFlexFlow").css('flex-direction', 'row-reverse');
+                    if(!($("#activeFF").is(':checked'))){
+                        $("#TestFlexFlow").css('flex-wrap', '');
+                    }
                 }else if(outils === "wrap"){
                     if(!($("#activeFF").is(':checked'))){
-                        console.log('checkbox')
                         $("#TestFlexFlow").css('flex-direction', '');
                     }
                     $("#TestFlexFlow").css('flex-wrap', 'wrap');
+                    if(!($("#activeFF").is(':checked'))){
+                        $("#TestFlexFlow").css('flex-direction', '');
+                    }
                 }else if(outils === "wraprev"){
                     $("#TestFlexFlow").css('flex-wrap', 'wrap-reverse');
+                    if(!($("#activeFF").is(':checked'))){
+                        $("#TestFlexFlow").css('flex-direction', '');
+                    }
                 }else if(outils === "nowrap"){
                     $("#TestFlexFlow").css('flex-wrap', 'nowrap');
-                // } else if(outils === "dirwrap"){
-                //     $("#TestFlexFlow").css('flex-flow', 'column-reverse wrap');
+                    if(!($("#activeFF").is(':checked'))){
+                        $("#TestFlexFlow").css('flex-direction', '');
+                    }    
                 }
             });
     });
