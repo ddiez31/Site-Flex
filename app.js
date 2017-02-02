a14b39cfbf7924c0bd9b09ec17c761c572d240f5
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-02-02 14:04:01 +0100

    indentage

--- app.js
+++ app.js
@@ -24,6 +24,34 @@ function draw() {
 
 $(document).ready(function() {
 
+	console.log('jquery fonctionne');
+    // Thibault start
+
+
+    $(".btnFlexFlow").click(function(){
+    	var outils = $(this).attr("id");
+    	console.log(outils);
+    	if(outils === "containercolrev"){
+    		$("#TestFlexFlow").css('flex-direction', '');
+    	}else if(outils === "containercol"){
+    		$("#TestFlexFlow").css('flex-direction', 'column');
+    	}else if(outils === "row"){
+    		$("#TestFlexFlow").css('flex-direction', 'row');
+    	}else if(outils === "row-reverse"){
+    		$("#TestFlexFlow").css('flex-direction', 'row-reverse');
+    	}else if(outils === "wrap"){
+    		if(!($("#activeFF").is(':checked'))){
+    			$("#TestFlexFlow").css('flex-direction', '');
+    		}
+    		$("#TestFlexFlow").css('flex-wrap', 'wrap');
+    	}else if(outils === "wraprev"){
+    		$("#TestFlexFlow").css('flex-wrap', 'wrap-reverse');
+    	}else if(outils === "nowrap"){
+    		$("#TestFlexFlow").css('flex-wrap', 'nowrap');
+            // } else if(outils === "dirwrap"){
+            //     $("#TestFlexFlow").css('flex-flow', 'column-reverse wrap');
+        }
+    });
 	// Cyril début,
 	$(".btnFlex").click(function(){
 		var inp;
@@ -41,36 +69,7 @@ $(document).ready(function() {
 		}
 	});
 	// Cyril Fin.
-
-        console.log('jquery fonctionne');
-        // Thibault start
-
-
-        $(".btnFlexFlow").click(function(){
-                var outils = $(this).attr("id");
-                console.log(outils);
-                if(outils === "containercolrev"){
-                    $("#TestFlexFlow").css('flex-direction', '');
-                }else if(outils === "containercol"){
-                    $("#TestFlexFlow").css('flex-direction', 'column');
-                }else if(outils === "row"){
-                    $("#TestFlexFlow").css('flex-direction', 'row');
-                }else if(outils === "row-reverse"){
-                    $("#TestFlexFlow").css('flex-direction', 'row-reverse');
-                }else if(outils === "wrap"){
-                    if(!($("#activeFF").is(':checked'))){
-                        $("#TestFlexFlow").css('flex-direction', '');
-                    }
-                    $("#TestFlexFlow").css('flex-wrap', 'wrap');
-                }else if(outils === "wraprev"){
-                    $("#TestFlexFlow").css('flex-wrap', 'wrap-reverse');
-                }else if(outils === "nowrap"){
-                    $("#TestFlexFlow").css('flex-wrap', 'nowrap');
-                // } else if(outils === "dirwrap"){
-                //     $("#TestFlexFlow").css('flex-flow', 'column-reverse wrap');
-                }
-            });
-    });
+});
 
 
 // Thibault end
