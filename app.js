b05917dc00b44816c1276de63e1bcc6d2e4c1fa5
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-02-03 12:21:08 +0100

    avant pull

--- app.js
+++ app.js
@@ -49,25 +49,38 @@ $(document).ready(function() {
 
 
 // Thibault end
-	// Cyril début, btn fonctionel pour le grow le shrink et basis
-	$(".btnFlex").click(function(){
-		var inp;
-		var outils = $(this).attr("id");
-		console.log(outils);
-		if(outils == "grow"){
-			inp = $('#growinp').val();
-			console.log(inp);
-			$("#groshrbas2").css('flex-grow', inp);
-		}else if(outils === "basis"){
-			inp = $('#basisinp').val();
-            console.log(inp);
-			$("#groshrbas2").css('flex-basis', inp);
-		} else if(outils === "shrink"){
-			inp = $('#shrinkinp').val();
-            console.log(inp);
-			$("#groshrbas3").css('flex-shrink', inp);
-		}
-	});
+	// Cyril début, btn fonctionel pour le grow
+	$(".inpgrowshrink").change(function(e){
+            var inp;
+            var outils = $(this).attr("id");
+            if(outils == "grow"){
+               inp = $('#growinp').val();
+               $("#grobas2").css('flex-grow', inp);
+           }else if(outils === "basis"){
+               inp = $('#basisinp').val();
+               $("#grobas3").css('flex-basis', inp);
+           } else if(outils === "shrink"){
+               inp = $('#shrinkinp').val();
+               $("#shr2").css('flex-shrink', inp);
+           }
+   });
 	// Cyril Fin.
+
+
+    //cyril et julien mettre des couleurs sur les ellements
+    var colorarray = ["blue","red","green","white","orange","yellow","purple", "lime", "Olive", "DeepPink", "GreenYellow", "MediumPurple", "OrangeRed", "Pink", "MidnightBlue", "MediumSpringGreen", "PapayaWhip", "PeachPuff", "LawnGreen", "Gold", "Fuchsia", "DarkMagenta", "Crimson"]
+    var prevrand = 8;
+    $('.element').each(function(){
+        var rand = Math.floor(Math.random()*colorarray.length);
+        if(rand === prevrand){
+            console.log(rand)
+            while(rand === prevrand){
+                rand = Math.floor(Math.random()*colorarray.length);
+            }
+        }
+        $(this).css("background-color", colorarray[rand]);
+        prevrand = rand;
+    })
+    //cyril et julien fin
 
 });
