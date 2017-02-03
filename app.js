6c1bb7b3f553c4f892c2bbd00372a0830ce641a8
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-02-03 23:54:31 +0100

    plein de petites corrections

--- app.js
+++ app.js
@@ -2,132 +2,131 @@ $(document).ready(function() {
     // Thibault start
 
     $(".btnFlexFlow").click(function() {
-        var outils = $(this).attr("id");
-        if (outils === "containercolrev") {
-            $("#TestFlexFlow").css('flex-direction', 'column-reverse');
-            if (!($("#activeFF").is(':checked'))) {
-                $("#TestFlexFlow").css('flex-wrap', '');
-            }
-        } else if (outils === "containercol") {
-            $("#TestFlexFlow").css('flex-direction', 'column');
-            if (!($("#activeFF").is(':checked'))) {
-                $("#TestFlexFlow").css('flex-wrap', '');
-            }
-        } else if (outils === "row") {
-            $("#TestFlexFlow").css('flex-direction', 'row');
-            if (!($("#activeFF").is(':checked'))) {
-                $("#TestFlexFlow").css('flex-wrap', '');
-            }
-        } else if (outils === "row-reverse") {
-            if (!($("#activeFF").is(':checked'))) {
-                $("#TestFlexFlow").css('flex-wrap', '');
-            }
-            $("#TestFlexFlow").css('flex-direction', 'row-reverse');
-            if (!($("#activeFF").is(':checked'))) {
-                $("#TestFlexFlow").css('flex-wrap', '');
-            }
-        } else if (outils === "wrap") {
-            if (!($("#activeFF").is(':checked'))) {
-                $("#TestFlexFlow").css('flex-direction', '');
-            }
-            $("#TestFlexFlow").css('flex-wrap', 'wrap');
-            if (!($("#activeFF").is(':checked'))) {
-                $("#TestFlexFlow").css('flex-direction', '');
-            }
-        } else if (outils === "wraprev") {
-            $("#TestFlexFlow").css('flex-wrap', 'wrap-reverse');
-            if (!($("#activeFF").is(':checked'))) {
-                $("#TestFlexFlow").css('flex-direction', '');
-            }
-        } else if (outils === "nowrap") {
-            $("#TestFlexFlow").css('flex-wrap', 'nowrap');
-            if (!($("#activeFF").is(':checked'))) {
-                $("#TestFlexFlow").css('flex-direction', '');
-            }
-        }
+    	var outils = $(this).attr("id");
+    	if (outils === "containercolrev") {
+    		$("#TestFlexFlow").css('flex-direction', 'column-reverse');
+    		if (!($("#activeFF").is(':checked'))) {
+    			$("#TestFlexFlow").css('flex-wrap', '');
+    		}
+    	} else if (outils === "containercol") {
+    		$("#TestFlexFlow").css('flex-direction', 'column');
+    		if (!($("#activeFF").is(':checked'))) {
+    			$("#TestFlexFlow").css('flex-wrap', '');
+    		}
+    	} else if (outils === "row") {
+    		$("#TestFlexFlow").css('flex-direction', 'row');
+    		if (!($("#activeFF").is(':checked'))) {
+    			$("#TestFlexFlow").css('flex-wrap', '');
+    		}
+    	} else if (outils === "row-reverse") {
+    		if (!($("#activeFF").is(':checked'))) {
+    			$("#TestFlexFlow").css('flex-wrap', '');
+    		}
+    		$("#TestFlexFlow").css('flex-direction', 'row-reverse');
+    		if (!($("#activeFF").is(':checked'))) {
+    			$("#TestFlexFlow").css('flex-wrap', '');
+    		}
+    	} else if (outils === "wrap") {
+    		if (!($("#activeFF").is(':checked'))) {
+    			$("#TestFlexFlow").css('flex-direction', '');
+    		}
+    		$("#TestFlexFlow").css('flex-wrap', 'wrap');
+    		if (!($("#activeFF").is(':checked'))) {
+    			$("#TestFlexFlow").css('flex-direction', '');
+    		}
+    	} else if (outils === "wraprev") {
+    		$("#TestFlexFlow").css('flex-wrap', 'wrap-reverse');
+    		if (!($("#activeFF").is(':checked'))) {
+    			$("#TestFlexFlow").css('flex-direction', '');
+    		}
+    	} else if (outils === "nowrap") {
+    		$("#TestFlexFlow").css('flex-wrap', 'nowrap');
+    		if (!($("#activeFF").is(':checked'))) {
+    			$("#TestFlexFlow").css('flex-direction', '');
+    		}
+    	}
 
     });
 
     // Thibault end
     // Cyril début, CURSEUR
     $(".btnFlex").click(function() {
-        var nb = $(this).attr("name");
-        var inp;
-        var outils = $(this).attr("type");
-        if (outils === "grow") {
-            inp = $('.growinp'+nb).val();
-            $("#grobas"+nb).css('flex-grow', inp);
-        } else if (outils === "basis") {
-            inp = $('.basisinp'+nb).val();
-            $("#grobas"+nb).css('flex-basis', inp+"px");
-        } else if (outils === "shrink") {
-            inp = $('#shrinkinp'+nb).val();
-            $("#shr"+nb).css('flex-shrink', inp);
-        }
+    	var nb = $(this).attr("name");
+    	var inp;
+    	var outils = $(this).attr("type");
+    	if (outils === "grow") {
+    		inp = $('.growinp'+nb).val();
+    		$("#grobas"+nb).css('flex-grow', inp);
+    	} else if (outils === "basis") {
+    		inp = $('.basisinp'+nb).val();
+    		$("#grobas"+nb).css('flex-basis', inp+"px");
+    	} else if (outils === "shrink") {
+    		inp = $('#shrinkinp'+nb).val();
+    		$("#shr"+nb).css('flex-shrink', inp);
+    	}
     });
     $("#TestFlexShrinkCurseur").change(function(){
-        var inp = ($("#TestFlexShrinkCurseur").val())*5;
-        console.log(inp);
-        $('#TestFlexShrink').css("width", inp)
-        $('#shrinkDivRange').html(inp)
+    	var inp = $("#TestFlexShrinkCurseur").val();
+    	$('#TestFlexShrink').css("width", inp);
+    	$('#shrinkDivRange').html("Modifier la largeur initiale: "+inp+"px");
     })
     // Cyril Fin.
 
 
     // David début
     $(".btnDisplay").click(function() {
-        if ($("#reset").click(function() {
-                $(".divDisplay").css("display", "");
-            }))
-            $(".divDisplay").css("display", "flex");
-    });
+    	if ($("#reset").click(function() {
+    		$(".divDisplay").css("display", "");
+    	}))
+    		$(".divDisplay").css("display", "flex");
+    	});
 
 
     $("#order").click(function() {
-        var inp;
-        inp = $('#orderinp1').val();
-        $("#orderbox1").css("order", inp);
-        inp = $('#orderinp2').val();
-        $("#orderbox2").css("order", inp);
-        inp = $('#orderinp3').val();
-        $("#orderbox3").css("order", inp);
-        inp = $('#orderinp4').val();
-        $("#orderbox4").css("order", inp);
+    	var inp;
+    	inp = $('#orderinp1').val();
+    	$("#orderbox1").css("order", inp);
+    	inp = $('#orderinp2').val();
+    	$("#orderbox2").css("order", inp);
+    	inp = $('#orderinp3').val();
+    	$("#orderbox3").css("order", inp);
+    	inp = $('#orderinp4').val();
+    	$("#orderbox4").css("order", inp);
     });
 
     $(".btnSelf").click(function() {
-        var outils = $(this).attr("id");
-        if (outils === "auto") {
-            $("#self3").css("height", '');
-            $("#self6").css("height", '');
-            $("#self3").css('align-self', 'auto');
-            $("#self6").css('align-self', 'auto');
-        } else if (outils === "start") {
-            $("#self3").css("height", '');
-            $("#self6").css("height", '');
-            $("#self3").css('align-self', 'flex-start');
-            $("#self6").css('align-self', 'flex-start');
-        } else if (outils === "end") {
-            $("#self3").css("height", '');
-            $("#self6").css("height", '');
-            $("#self3").css('align-self', 'flex-end');
-            $("#self6").css('align-self', 'flex-end');
-        } else if (outils === "center") {
-            $("#self3").css("height", '');
-            $("#self6").css("height", '');
-            $("#self3").css('align-self', 'center');
-            $("#self6").css('align-self', 'center');
-        } else if (outils === "baseline") {
-            $("#self3").css("height", '');
-            $("#self6").css("height", '');
-            $("#self3").css('align-self', 'baseline');
-            $("#self6").css('align-self', 'baseline');
-        } else if (outils === "stretch") {
-            $("#self3").css('align-self', 'stretch');
-            $("#self6").css('align-self', 'stretch');
-            $("#self3").css("height", 'inherit');
-            $("#self6").css("height", 'inherit');
-        }
+    	var outils = $(this).attr("id");
+    	if (outils === "auto") {
+    		$("#self3").css("height", '');
+    		$("#self6").css("height", '');
+    		$("#self3").css('align-self', 'auto');
+    		$("#self6").css('align-self', 'auto');
+    	} else if (outils === "start") {
+    		$("#self3").css("height", '');
+    		$("#self6").css("height", '');
+    		$("#self3").css('align-self', 'flex-start');
+    		$("#self6").css('align-self', 'flex-start');
+    	} else if (outils === "end") {
+    		$("#self3").css("height", '');
+    		$("#self6").css("height", '');
+    		$("#self3").css('align-self', 'flex-end');
+    		$("#self6").css('align-self', 'flex-end');
+    	} else if (outils === "center") {
+    		$("#self3").css("height", '');
+    		$("#self6").css("height", '');
+    		$("#self3").css('align-self', 'center');
+    		$("#self6").css('align-self', 'center');
+    	} else if (outils === "baseline") {
+    		$("#self3").css("height", '');
+    		$("#self6").css("height", '');
+    		$("#self3").css('align-self', 'baseline');
+    		$("#self6").css('align-self', 'baseline');
+    	} else if (outils === "stretch") {
+    		$("#self3").css('align-self', 'stretch');
+    		$("#self6").css('align-self', 'stretch');
+    		$("#self3").css("height", 'inherit');
+    		$("#self6").css("height", 'inherit');
+    	}
     });
 
 
@@ -135,14 +134,29 @@ $(document).ready(function() {
     var colorarray = ["blue", "red", "green", "white", "orange", "yellow", "purple", "lime", "Olive", "DeepPink", "GreenYellow", "MediumPurple", "OrangeRed", "Pink", "MidnightBlue", "MediumSpringGreen", "PapayaWhip", "PeachPuff", "LawnGreen", "Gold", "Fuchsia", "DarkMagenta", "Crimson"]
     var prevrand = (colorarray.length)+1;
     $('.element').each(function() {
-            var rand = Math.floor(Math.random() * colorarray.length);
-            if (rand === prevrand) {
-                while (rand === prevrand) {
-                    rand = Math.floor(Math.random() * colorarray.length);
-                }
-            }
-            $(this).css("background-color", colorarray[rand]);
-            prevrand = rand;
-        })
+    	var rand = Math.floor(Math.random() * colorarray.length);
+    	if (rand === prevrand) {
+    		while (rand === prevrand) {
+    			rand = Math.floor(Math.random() * colorarray.length);
+    		}
+    	}
+    	$(this).css("background-color", colorarray[rand]);
+    	prevrand = rand;
+    })
         //cyril et julien fin
+
+    //début justify-Content et align-content
+
+    $(".btnFlexJustifAlign").click(function() { 
+    	var outils1 = $(this).attr("name");
+    	var outils = $(this).attr("type");
+    	if(outils1 == "justify-content" || outils1 == "align-items"){
+    		console.log(outils1);
+       		$("#TestFlexJustifAlign").css(outils1, outils);
+    	}else if(outils1 == "align-content"){
+    		console.log(outils1);
+    		$("#TestFlexAlign").css(outils1, outils);
+    	}
+    });
+    //fin justify-Content et align-content
 });
